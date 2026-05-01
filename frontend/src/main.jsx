60e0dbd864d42a708ad6acb1ddd7457998236876
MD.SAJEDUL ISLAM <m.sajid1495@gmail.com>
2026-05-01 17:19:14 +0600

    cleaning repo

--- frontend/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <div>Frontend is running!</div>
-  </React.StrictMode>
-);
